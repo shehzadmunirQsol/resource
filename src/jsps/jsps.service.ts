601f6fbf16accc6654941c04f7e686fc8bfc989a
shehzadINPL <shehzad.munir@innovative-net.com>
2024-07-30 18:29:43 +0500

    work on jsp skills and levels jspskills module

--- src/jsps/jsps.service.ts
+++ src/jsps/jsps.service.ts
@@ -8,15 +8,48 @@ export class JspsService {
   constructor(private readonly db: DatabaseService) {}
   async create(createJspDto: any) {
     console.log({ createJspDto });
+    const { expected, ...payload } = createJspDto;
     // await this.db.jsp.create({ data: createJspDto });
-    return await this.db.jsp.create({ data: createJspDto });
+    return await this.db.jsp.create({
+      data: {
+        ...payload,
+        JspSkill: { createMany: { data: expected } },
+      },
+    });
   }
 
   async findAll() {
     return await this.db.jsp.findMany({
       select: {
-        Skills: true,
+        JspSkill: {
+          select: {
+            Skill: {
+              select: {
+                SkillLevels: {
+                  select: {
+                    _count: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+        // SkillLevels: true,
       },
+      // include: {
+      //   Role: true,
+      //   Competency: true,
+      //   JspSkill: {
+      //     select: {
+      //       ExpectedLevel: true,
+      //       Skill: {
+      //         select: {
+      //           SkillLevels: true,
+      //         },
+      //       },
+      //     },
+      //   },
+      // },
     });
   }
 
