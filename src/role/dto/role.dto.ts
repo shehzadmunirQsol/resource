2a603130a12a86a8c74997efdffdc04f0add92dd
shehzadINPL <shehzad.munir@innovative-net.com>
2024-08-08 18:56:04 +0500

    work on roles

--- src/role/dto/role.dto.ts
+++ src/role/dto/role.dto.ts
@@ -2,10 +2,12 @@ import { IsInt, IsOptional, IsString } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class FindAllDto {
+  @IsOptional()
   @IsInt()
   @Type(() => Number)
   first: number;
 
+  @IsOptional()
   @IsInt()
   @Type(() => Number)
   rows: number;
